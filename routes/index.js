986501f4b10e3a5a19cf7af029d1cffdbb6f0990
Steven <Sphan510@gmail.com>
2017-11-07 01:25:06 -0800

    Clean Slate

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//########################################
+
+//to process data sent in on request need body-parser module
+
+var bodyParser = require('body-parser');
+var path = require ('path'); //to work with separtors on any OS including Windows
+var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
+
+router.use(bodyParser.json()); // for parsing application/json
+router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
+//#########################################
+
+router.post('/', function(req, res, next) {
+    var value_name = req.body.name;
+    res.send("hello " + value_name);
 });
 
 module.exports = router;
